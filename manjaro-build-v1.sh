658125f367f4b6323ddab5d1e7dcae293e66008d
Seth Dawson <seth.dawson@hotmail.com>
2020-11-14 19:00:01 -0600

    Added scripts

---
+++ manjaro-build-v1.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#https://wiki.manjaro.org/index.php/Buildiso_with_AUR_packages:_Using_buildpkg
+
+branch="x86_64"
+
+destination1="/home/seth/SMDLINUX-REPO/smdlinux_repo/x86_64/"
+destination2="/home/seth/SMDLINUX-REPO/smdlinux_repo_3party/x86_64/"
+destination3="/home/seth/SMDLINUX-REPO/smdlinux_repo_testing/x86_64/"
+
+destiny=$destination1
+
+search=smdlinux
+
+# Give a comment to the commit if you want
+echo "Enter Package Name"
+
+read input
+
+buildpkg -s -p $input
+
+echo ""
+echo "Moving created files to " $destiny
+echo ""
+mv /var/cache/manjaro-tools/pkg/stable/$branch/$search*pkg.tar.zst $destiny
+mv /var/cache/manjaro-tools/pkg/stable/$branch/$search*pkg.tar.zst.sig $destiny
+
+echo "###########################################################################"
+echo "###################           Cleaning Up            ######################"
+echo "###########################################################################"
+
+sudo rm -r /var/lib/manjaro-tools/buildpkg
+rm -rf ~/pkgbuild/$input
+
+echo ""
+echo "###########################################################################"
+echo "###################          build finished          ######################"
+echo "###########################################################################"
